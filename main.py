bce2afa7d8c695a1f7bfdc8b7e78501d0d578188
Neeraj Kanoi <neeraj.kanoi@gmail.com>
2021-01-13 11:55:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ while cam.isOpened():
         winsound.PlaySound('alert.wav', winsound.SND_ASYNC)
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('Granny Cam', frame1)
+    cv2.imshow('Neeraj\'s Cam', frame1)
